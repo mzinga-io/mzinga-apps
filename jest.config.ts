1b38b3fc668a1e733fa7139001e8fce7e0029e83
Alberto Maghini <a.maghini@gmail.com>
2025-10-22 13:59:31 +0200

    chore: updated jest reference to 30.x (#116)
    * chore: updated jest reference to 30.x
    
    * fix: moved to require

--- jest.config.ts
+++ jest.config.ts
@@ -91,6 +91,7 @@ export default {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "\\.(scss|css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "^mzinga(.*)$": "<rootDir>/node_modules/mzinga$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
