a8b8d4e85b8043337fb6e1b05616c43aabcdc95d
Alberto Maghini <a.maghini@gmail.com>
2026-04-10 15:36:17 +0200

    test: reviewed messageBusService

--- __tests__/unit-tests/services/MessageBusService.test.ts
+++
@@ -1,9 +0,0 @@
-import { messageBusService } from "../../../src/messageBusService";
-
-describe("services", () => {
-  describe("MessageBusService", () => {
-    it("Should return false for null connection", () => {
-      expect(messageBusService.isConnected()).toBeFalsy();
-    });
-  });
-});
